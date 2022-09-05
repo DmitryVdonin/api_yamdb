0921874eaad04df59d914a6ecf3722b217d804f1
Дмитрий Вдонин <dvdonin@rambler.ru>
2022-09-05 20:14:10 +0300

    добавил verbose_name и поле author для модели comments

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -15,15 +15,21 @@ CHOICES = (
     (10, 10),
 )
 
+
 class Review(models.Model):
     """Модель Review(отзыв)."""
 
-    title_id = models.ForeignKey(Titles, on_delete=models.CASCADE)
-    text = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    score = models.IntegerField(choices=CHOICES)
-    pub_date = models.DateTimeField(auto_now_add=True)
-
+    title_id = models.ForeignKey(
+        Titles, on_delete=models.CASCADE,
+        verbose_name='Произведение',
+    )
+    text = models.TextField(verbose_name='Текст отзыва')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор отзыва',
+    )
+    score = models.IntegerField(choices=CHOICES, verbose_name='Оценка')
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
 
     def __str__(self):
         return self.text[:CHARS_PER_STR]
@@ -32,10 +38,16 @@ class Review(models.Model):
 class Comments(models.Model):
     """Модель Comments(комментарии)."""
 
-    review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-
+    review_id = models.ForeignKey(
+        Review, on_delete=models.CASCADE,
+        verbose_name='Отзыв',
+    )
+    text = models.TextField(verbose_name='Текст комментария')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор комментария',
+    )
+    pub_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата')
 
     def __str__(self):
         return self.text[:CHARS_PER_STR]
