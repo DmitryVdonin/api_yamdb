c69a3f284f63f2f7914b79607717cd8d70797ca7
Дмитрий Вдонин <dvdonin@rambler.ru>
2022-09-07 13:33:19 +0300

    parser_done

--- api_yamdb/core/management/commands/db_filling.py
+++ api_yamdb/core/management/commands/db_filling.py
@@ -6,11 +6,9 @@ from reviews.models import Review, Category, Comments, Title, User, GenreTitle, 
 
 
 class Command(BaseCommand):
+    """"Добавляет данные в базу из csv файлов."""
 
     def handle(self, *args, **kwargs):
-
-            #'static/data/comments.csv': Comments,
-            #'static/data/review.csv': Review,
 
         def db_fill(csv_file, model, fk_1=None, linked_model_1=None, fk_2=None, linked_model_2=None):
             with open(csv_file, encoding='utf-8') as r_file:
@@ -33,3 +31,6 @@ class Command(BaseCommand):
         db_fill('static/data/titles.csv', Title, 3, Category)
         db_fill('static/data/genre_title.csv', GenreTitle)
         db_fill('static/data/users.csv', User)
+        db_fill('static/data/review.csv', Review, 1, Title, 3, User)
+        db_fill('static/data/comments.csv', Comments, 1, Review, 3, User)
+
