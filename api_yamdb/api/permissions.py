78cabfb076d101f7e1d74278bad2097b14c354b2
Дмитрий Вдонин <dvdonin@rambler.ru>
2022-09-08 19:24:31 +0300

    isadminorreadonly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -5,12 +5,19 @@ class IsAdmin(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-
         return (
             request.user.is_authenticated
             and
             (request.user.role == 'admin' or request.user.is_superuser)
         )
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or request.user.role == 'admin'
+            or request.user.is_superuser)
 
 
 class IsOwnerOrModeratorOrReadOnly(permissions.BasePermission):
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from .serializers import (CategorySerializer, GenreSerializer, TitleSerializer,
                           UserSerializer, UserCreateSerializer,
-                          ReadOnlyTitleSerializer)
+                          ReadOnlyTitleSerializer, ReviewSerializer)
 from .permissions import IsAdmin, IsOwnerOrModeratorOrReadOnly
 from reviews.token_generator import confirmation_code
 from reviews.models import Category, Genre, Title, User
